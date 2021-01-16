ce89b329bf56514dc69ca0f0c5d6441beda1e7cf
richalljangi <richall-jangi@hotmail.com>
2021-01-16 21:35:45 +0100

    advies pagina functionaliteit

--- src/app/advies/advies.page.ts
+++ src/app/advies/advies.page.ts
@@ -6,8 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./advies.page.scss'],
 })
 export class AdviesPage implements OnInit {
+     constructor() {
+   value: int = 240;
+   adviesEen: boolean =  false;
+   adviesTwee: boolean = false;
+   adviesDrie: boolean = false;
+   adviesVier: boolean = false;
+   AdviesVijf: boolean = false;
 
-  constructor() { }
+   if(value<200)
+     {
+         adviesEen = true;
+     }
+
+   if (value>=200 && value<280)
+     {
+         adviesTwee = true;
+     }
+     if (value>=280 && value<350)
+     {
+         adviesDrie = true;
+     }
+     if (value>=350 && value <450)
+     {
+         adviesVier = true;
+     }
+     if(value>450)
+     {
+        adviesVijf = true ;
+     }
+
+   }
+
 
   ngOnInit() {
   }
