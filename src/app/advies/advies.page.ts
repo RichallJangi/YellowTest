10208fcddcbf23a4c52b6c34081330b219e4b1c1
Casper Bastiaan-Net <casper.bastiaan.net@gmail.com>
2021-01-20 19:21:42 +0100

    advice visible

--- src/app/advies/advies.page.ts
+++ src/app/advies/advies.page.ts
@@ -7,7 +7,9 @@ import {DatabaseService} from '../services/database.service';
   styleUrls: ['./advies.page.scss'],
 })
 export class AdviesPage implements OnInit {
-     constructor(private database: DatabaseService, private zone: NgZone) {}
+  constructor(private database: DatabaseService, private zone: NgZone) {}
+  advies = 'wordt ingelezen';
+  meting = 'wordt ingelezen';
   adviesEen =  false;
   adviesTwee = false;
   adviesDrie = false;
@@ -22,6 +24,7 @@ export class AdviesPage implements OnInit {
               measurement = data.rows.item(i);
           }
           this.setAdvice(measurement.BAC);
+          this.meting = measurement.BAC;
           console.log('the measurement is ' + JSON.stringify(measurement));
           console.log('' + this.adviesEen + this.adviesTwee + this.adviesDrie + this.adviesVier + this.adviesVijf);
       });
@@ -31,23 +34,23 @@ export class AdviesPage implements OnInit {
       this.zone.run(() => {
           if (value < 200 )
           {
-            this.adviesEen = true;
+            this.advies = 'Nuchter';
           }
           if (value >= 200 && value < 280)
           {
-            this.adviesTwee = true;
+            this.advies = 'Neem nog een glaasje';
           }
           if (value >= 280 && value < 350)
           {
-            this.adviesDrie = true;
+            this.advies = 'Oke nog eentje dan';
           }
           if (value >= 350 && value < 450)
           {
-            this.adviesVier = true;
+            this.advies = 'Gaat fout';
           }
           if (value > 450 )
           {
-            this.adviesVijf = true ;
+            this.advies = 'Ladderzat' ;
           }
       });
     }
